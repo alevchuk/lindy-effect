47fb4a722223be93b3a8ca451da6ae7b0b682b0c
Aleksandr Levchuk <alevchuk@gmail.com>
2019-09-09 06:43:02 -0700

    convert raw data to Date type

--- process.R
+++ process.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
-mydata <- read.csv("gldt.csv", header = FALSE)
+mydata <- read.csv("gldt.csv", header=F, stringsAsFactors=F)
 
 # name columns
-colnames(mydata) <- c('Type', 'Name', NA, 'Parent', 'Start', 'Stop', NA, 'Description')
+colnames(mydata) <- c('Type', 'Name', NA, 'Parent', 'Start', 'Stop')
 
 # drop unused columns
 mydata <- mydata[,-which(colnames(mydata) %in% NA)]
@@ -11,4 +11,31 @@ mydata <- mydata[,-which(colnames(mydata) %in% NA)]
 # drop unused rows
 mydata <- mydata[mydata$Type == 'N', ]
 
-tail(mydata)
+# Convert to date type
+for ( col in c("Start", "Stop") ) {
+	# to covert to a data type we need year, month, and day
+	# year only entries would case a conversion error
+	year_only_rows <- grep("^[0-9]{4}$", mydata[,col])
+	mydata[year_only_rows, col] <-
+		paste(mydata[year_only_rows, col], ".1.1", sep="")
+
+	# to covert to a data type we need year, month, and day
+	# year.month only entries would case a conversion error
+	month_only_rows <- grep("^[0-9]{4}\\.[0-9]+$", mydata[,col])
+	mydata[month_only_rows, col] <-
+		paste(mydata[month_only_rows, col], ".1", sep="")
+
+	# some entires have day as "0"
+	# this would case a conversion error
+	mydata[,col] <- sub("^([0-9]{4}\\.[0-9]+)\\.0+$", "\\1.1", mydata[,col])
+
+	# Convert
+	mydata[,col] <- as.Date(mydata[,col], "%Y.%m.%d")
+}
+
+# Print ranges
+data.frame(
+	"Start Range"=range(mydata$Start, na.rm=T),
+     	"Stop Range"=range(mydata$Stop, na.rm=T),
+	row.names=c("min", "max")
+)
