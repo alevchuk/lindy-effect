c6f4ecd4ef55b716f1a7a8e3f2474fcddbfb4c7e
Aleksandr Levchuk <alevchuk@gmail.com>
2019-09-09 07:09:41 -0700

    implement getSurvivorsAt

--- process.R
+++ process.R
@@ -13,20 +13,20 @@ mydata <- mydata[mydata$Type == 'N', ]
 
 # Convert to date type
 for ( col in c("Start", "Stop") ) {
-	# to covert to a data type we need year, month, and day
+	# to convert to a data type we need year, month, and day
 	# year only entries would case a conversion error
 	year_only_rows <- grep("^[0-9]{4}$", mydata[,col])
 	mydata[year_only_rows, col] <-
 		paste(mydata[year_only_rows, col], ".1.1", sep="")
 
-	# to covert to a data type we need year, month, and day
+	# to convert to a data type we need year, month, and day
 	# year.month only entries would case a conversion error
 	month_only_rows <- grep("^[0-9]{4}\\.[0-9]+$", mydata[,col])
 	mydata[month_only_rows, col] <-
 		paste(mydata[month_only_rows, col], ".1", sep="")
 
-	# some entires have day as "0"
-	# this would case a conversion error
+	# some entries have day as "0"
+	# this would cause a conversion error
 	mydata[,col] <- sub("^([0-9]{4}\\.[0-9]+)\\.0+$", "\\1.1", mydata[,col])
 
 	# Convert
@@ -39,3 +39,10 @@ data.frame(
      	"Stop Range"=range(mydata$Stop, na.rm=T),
 	row.names=c("min", "max")
 )
+
+getSurvivorsAt <- function(df, at_time) {
+	df[df$Start < at_time & (df$Stop > at_time | is.na(df$Stop)),]
+}
+
+cat("\nSurvivors at 1994-01-01\n")
+getSurvivorsAt(mydata, "1994-01-01")
