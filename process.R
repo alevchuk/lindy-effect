8adbe4f3d1215f69b2805f55d4a282827b1dfda1
Aleksandr Levchuk <alevchuk@gmail.com>
2019-09-11 07:54:38 -0700

    get survivors for all time points

--- process.R
+++ process.R
@@ -1,48 +1,65 @@
 #!/usr/bin/env Rscript
 
-mydata <- read.csv("gldt.csv", header=F, stringsAsFactors=F)
+inputData <- read.csv("gldt.csv", header=F, stringsAsFactors=F)
 
 # name columns
-colnames(mydata) <- c('Type', 'Name', NA, 'Parent', 'Start', 'Stop')
+colnames(inputData) <- c('Type', 'Name', NA, 'Parent', 'Start', 'Stop')
 
 # drop unused columns
-mydata <- mydata[,-which(colnames(mydata) %in% NA)]
+inputData <- inputData[,-which(colnames(inputData) %in% NA)]
 
 # drop unused rows
-mydata <- mydata[mydata$Type == 'N', ]
+inputData <- inputData[inputData$Type == 'N', ]
 
 # Convert to date type
 for ( col in c("Start", "Stop") ) {
-	# to convert to a data type we need year, month, and day
-	# year only entries would case a conversion error
-	year_only_rows <- grep("^[0-9]{4}$", mydata[,col])
-	mydata[year_only_rows, col] <-
-		paste(mydata[year_only_rows, col], ".1.1", sep="")
+        # to convert to a data type we need year, month, and day
+        # year only entries would case a conversion error
+        year_only_rows <- grep("^[0-9]{4}$", inputData[,col])
+        inputData[year_only_rows, col] <-
+                paste(inputData[year_only_rows, col], ".1.1", sep="")
 
-	# to convert to a data type we need year, month, and day
-	# year.month only entries would case a conversion error
-	month_only_rows <- grep("^[0-9]{4}\\.[0-9]+$", mydata[,col])
-	mydata[month_only_rows, col] <-
-		paste(mydata[month_only_rows, col], ".1", sep="")
+        # to convert to a data type we need year, month, and day
+        # year.month only entries would case a conversion error
+        month_only_rows <- grep("^[0-9]{4}\\.[0-9]+$", inputData[,col])
+        inputData[month_only_rows, col] <-
+                paste(inputData[month_only_rows, col], ".1", sep="")
 
-	# some entries have day as "0"
-	# this would cause a conversion error
-	mydata[,col] <- sub("^([0-9]{4}\\.[0-9]+)\\.0+$", "\\1.1", mydata[,col])
+        # some entries have day as "0"
+        # this would cause a conversion error
+        inputData[,col] <- sub("^([0-9]{4}\\.[0-9]+)\\.0+$", "\\1.1", inputData[,col])
 
-	# Convert
-	mydata[,col] <- as.Date(mydata[,col], "%Y.%m.%d")
+        # Convert
+        inputData[,col] <- as.Date(inputData[,col], "%Y.%m.%d")
+}
+
+getSurvivorsAt <- function(df, atTimePoints) {
+        lapply(
+                atTimePoints,
+                function(atTimePoint) {
+                        df[df$Start < atTimePoint & (df$Stop > atTimePoint | is.na(df$Stop)),]
+                }
+        )
 }
 
 # Print ranges
 data.frame(
-	"Start Range"=range(mydata$Start, na.rm=T),
-     	"Stop Range"=range(mydata$Stop, na.rm=T),
-	row.names=c("min", "max")
+        "Start Range"=range(inputData$Start, na.rm=T),
+        "Stop Range"=range(inputData$Stop, na.rm=T),
+        row.names=c("min", "max")
 )
 
-getSurvivorsAt <- function(df, at_time) {
-	df[df$Start < at_time & (df$Stop > at_time | is.na(df$Stop)),]
-}
+# Run the experiment up to the time point where
+# we can still know if the survivors will live
+# twice their current age
+dataGoesUpTo = range(inputData$Stop, na.rm=T)[[2]]
+experimentRangeStart = range(inputData$Start, na.rm=T)[[1]]
+experimentRangeStop = experimentRangeStart + abs(difftime(experimentRangeStart, dataGoesUpTo) / 2)
 
-cat("\nSurvivors at 1994-01-01\n")
-getSurvivorsAt(mydata, "1994-01-01")
+experimentResults <- data.frame(
+        "currentTime"=seq(experimentRangeStart, experimentRangeStop, by="month")
+)
+
+survivors <- getSurvivorsAt(inputData, experimentResults$currentTime)
+experimentResults$numSurvivoursCount <- sapply(survivors, nrow)
+experimentResults
